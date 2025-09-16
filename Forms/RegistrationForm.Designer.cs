754dc363ffd7ed43de41fc399ec69db4f5e1a764
Mrx™ <itzrakib34@gmail.com>
2025-09-17 03:09:52 +0600

    little fix reg

--- Forms/RegistrationForm.Designer.cs
+++ Forms/RegistrationForm.Designer.cs
@@ -37,7 +37,6 @@
             this.registerbutton = new System.Windows.Forms.Button();
             this.cancelbutton = new System.Windows.Forms.Button();
             this.rolecomboBox = new System.Windows.Forms.ComboBox();
-            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // usernametext
@@ -114,25 +113,11 @@
             this.rolecomboBox.Size = new System.Drawing.Size(573, 24);
             this.rolecomboBox.TabIndex = 14;
             // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.BackColor = System.Drawing.Color.Transparent;
-            this.label8.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label8.Location = new System.Drawing.Point(549, 19);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(212, 31);
-            this.label8.TabIndex = 16;
-            this.label8.Text = "Register Account";
-            this.label8.Click += new System.EventHandler(this.label8_Click);
-            // 
             // RegistrationForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.BackgroundImage = global::Store.Properties.Resources.register;
             this.ClientSize = new System.Drawing.Size(1317, 728);
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.rolecomboBox);
             this.Controls.Add(this.cancelbutton);
             this.Controls.Add(this.registerbutton);
@@ -161,6 +146,5 @@
         private System.Windows.Forms.Button registerbutton;
         private System.Windows.Forms.Button cancelbutton;
         private System.Windows.Forms.ComboBox rolecomboBox;
-        private System.Windows.Forms.Label label8;
     }
 }
