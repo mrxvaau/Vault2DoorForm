a3be45b3f0d64bb0bcc6848d623390dd883af078
Mrx™ <itzrakib34@gmail.com>
2025-09-17 03:34:37 +0600

    cart tested and done

--- Forms/CartForm.Designer.cs
+++ Forms/CartForm.Designer.cs
@@ -33,11 +33,6 @@
             this.lblQuantity = new System.Windows.Forms.Label();
             this.lblTotal = new System.Windows.Forms.Label();
             this.btnProceed = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -45,99 +40,67 @@
             // lblProuct
             // 
             this.lblProuct.AutoSize = true;
-            this.lblProuct.Location = new System.Drawing.Point(395, 126);
+            this.lblProuct.BackColor = System.Drawing.Color.Transparent;
+            this.lblProuct.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProuct.ForeColor = System.Drawing.Color.Khaki;
+            this.lblProuct.Location = new System.Drawing.Point(660, 289);
             this.lblProuct.Name = "lblProuct";
-            this.lblProuct.Size = new System.Drawing.Size(44, 16);
+            this.lblProuct.Size = new System.Drawing.Size(86, 32);
             this.lblProuct.TabIndex = 0;
             this.lblProuct.Text = "label1";
             // 
             // lblKarat
             // 
             this.lblKarat.AutoSize = true;
-            this.lblKarat.Location = new System.Drawing.Point(395, 176);
+            this.lblKarat.BackColor = System.Drawing.Color.Transparent;
+            this.lblKarat.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold);
+            this.lblKarat.ForeColor = System.Drawing.Color.Khaki;
+            this.lblKarat.Location = new System.Drawing.Point(660, 365);
             this.lblKarat.Name = "lblKarat";
-            this.lblKarat.Size = new System.Drawing.Size(44, 16);
+            this.lblKarat.Size = new System.Drawing.Size(86, 32);
             this.lblKarat.TabIndex = 1;
             this.lblKarat.Text = "label2";
             // 
             // lblQuantity
             // 
             this.lblQuantity.AutoSize = true;
-            this.lblQuantity.Location = new System.Drawing.Point(395, 230);
+            this.lblQuantity.BackColor = System.Drawing.Color.Transparent;
+            this.lblQuantity.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold);
+            this.lblQuantity.ForeColor = System.Drawing.Color.Khaki;
+            this.lblQuantity.Location = new System.Drawing.Point(660, 430);
             this.lblQuantity.Name = "lblQuantity";
-            this.lblQuantity.Size = new System.Drawing.Size(44, 16);
+            this.lblQuantity.Size = new System.Drawing.Size(86, 32);
             this.lblQuantity.TabIndex = 2;
             this.lblQuantity.Text = "label3";
             // 
             // lblTotal
             // 
             this.lblTotal.AutoSize = true;
-            this.lblTotal.Location = new System.Drawing.Point(395, 277);
+            this.lblTotal.BackColor = System.Drawing.Color.Transparent;
+            this.lblTotal.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Bold);
+            this.lblTotal.ForeColor = System.Drawing.Color.Khaki;
+            this.lblTotal.Location = new System.Drawing.Point(656, 496);
             this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(44, 16);
+            this.lblTotal.Size = new System.Drawing.Size(86, 32);
             this.lblTotal.TabIndex = 3;
             this.lblTotal.Text = "label4";
             // 
             // btnProceed
             // 
-            this.btnProceed.Location = new System.Drawing.Point(289, 344);
+            this.btnProceed.BackColor = System.Drawing.Color.Lime;
+            this.btnProceed.Location = new System.Drawing.Point(534, 555);
             this.btnProceed.Name = "btnProceed";
-            this.btnProceed.Size = new System.Drawing.Size(183, 33);
+            this.btnProceed.Size = new System.Drawing.Size(244, 64);
             this.btnProceed.TabIndex = 4;
             this.btnProceed.Text = "Proced to Payment";
-            this.btnProceed.UseVisualStyleBackColor = true;
+            this.btnProceed.UseVisualStyleBackColor = false;
             this.btnProceed.Click += new System.EventHandler(this.btnProceed_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(274, 126);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(93, 16);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Product Name";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(329, 176);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 16);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Karat";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(312, 230);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 16);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Quantity";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(330, 277);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(37, 16);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "price";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(272, 25);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(200, 31);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Check Your Cart";
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::Store.Properties.Resources.Back;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(1052, 163);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(51, 44);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -147,15 +110,10 @@
             // 
             // CartForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::Store.Properties.Resources.cart;
+            this.ClientSize = new System.Drawing.Size(1187, 695);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnProceed);
             this.Controls.Add(this.lblTotal);
             this.Controls.Add(this.lblQuantity);
@@ -178,11 +136,6 @@
         private System.Windows.Forms.Label lblQuantity;
         private System.Windows.Forms.Label lblTotal;
         private System.Windows.Forms.Button btnProceed;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
