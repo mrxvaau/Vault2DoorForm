0b5e0f3da2aef060d13e1f5a007f82b2396fac27
Mrx™ <itzrakib34@gmail.com>
2025-09-17 03:27:40 +0600

    payment done and tested

--- Forms/PaymentForm.Designer.cs
+++ Forms/PaymentForm.Designer.cs
@@ -33,9 +33,6 @@
             this.radioBkash = new System.Windows.Forms.RadioButton();
             this.radioNagad = new System.Windows.Forms.RadioButton();
             this.radiocash = new System.Windows.Forms.RadioButton();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -46,89 +43,71 @@
             // 
             // lblAmount
             // 
-            this.lblAmount.AutoSize = true;
-            this.lblAmount.Location = new System.Drawing.Point(421, 91);
+            this.lblAmount.BackColor = System.Drawing.Color.Transparent;
+            this.lblAmount.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmount.ForeColor = System.Drawing.Color.Yellow;
+            this.lblAmount.Location = new System.Drawing.Point(688, 278);
             this.lblAmount.Name = "lblAmount";
-            this.lblAmount.Size = new System.Drawing.Size(51, 16);
+            this.lblAmount.Size = new System.Drawing.Size(128, 37);
             this.lblAmount.TabIndex = 0;
             this.lblAmount.Text = "amount";
             // 
             // btnPlaceOrder
             // 
-            this.btnPlaceOrder.Location = new System.Drawing.Point(345, 330);
+            this.btnPlaceOrder.BackColor = System.Drawing.Color.Lime;
+            this.btnPlaceOrder.Location = new System.Drawing.Point(569, 546);
             this.btnPlaceOrder.Name = "btnPlaceOrder";
-            this.btnPlaceOrder.Size = new System.Drawing.Size(111, 59);
+            this.btnPlaceOrder.Size = new System.Drawing.Size(185, 59);
             this.btnPlaceOrder.TabIndex = 1;
             this.btnPlaceOrder.Text = "Place Order";
-            this.btnPlaceOrder.UseVisualStyleBackColor = true;
+            this.btnPlaceOrder.UseVisualStyleBackColor = false;
             this.btnPlaceOrder.Click += new System.EventHandler(this.btnPlaceOrder_Click);
             // 
             // radioBkash
             // 
             this.radioBkash.AutoSize = true;
-            this.radioBkash.Location = new System.Drawing.Point(182, 262);
+            this.radioBkash.BackColor = System.Drawing.Color.Transparent;
+            this.radioBkash.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.radioBkash.Location = new System.Drawing.Point(423, 483);
             this.radioBkash.Name = "radioBkash";
             this.radioBkash.Size = new System.Drawing.Size(66, 20);
             this.radioBkash.TabIndex = 2;
             this.radioBkash.TabStop = true;
             this.radioBkash.Text = "Bkash";
-            this.radioBkash.UseVisualStyleBackColor = true;
+            this.radioBkash.UseVisualStyleBackColor = false;
             this.radioBkash.CheckedChanged += new System.EventHandler(this.radioBkash_CheckedChanged);
             // 
             // radioNagad
             // 
             this.radioNagad.AutoSize = true;
-            this.radioNagad.Location = new System.Drawing.Point(356, 262);
+            this.radioNagad.BackColor = System.Drawing.Color.Transparent;
+            this.radioNagad.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.radioNagad.Location = new System.Drawing.Point(597, 483);
             this.radioNagad.Name = "radioNagad";
             this.radioNagad.Size = new System.Drawing.Size(70, 20);
             this.radioNagad.TabIndex = 3;
             this.radioNagad.TabStop = true;
             this.radioNagad.Text = "Nagad";
-            this.radioNagad.UseVisualStyleBackColor = true;
+            this.radioNagad.UseVisualStyleBackColor = false;
             // 
             // radiocash
             // 
             this.radiocash.AutoSize = true;
-            this.radiocash.Location = new System.Drawing.Point(519, 262);
+            this.radiocash.BackColor = System.Drawing.Color.Transparent;
+            this.radiocash.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.radiocash.Location = new System.Drawing.Point(760, 483);
             this.radiocash.Name = "radiocash";
             this.radiocash.Size = new System.Drawing.Size(130, 20);
             this.radiocash.TabIndex = 4;
             this.radiocash.TabStop = true;
             this.radiocash.Text = "Cash on Delivery";
-            this.radiocash.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(311, 91);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 16);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "You have to pay";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(301, 155);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(179, 16);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "Choose how you want to pay ";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(293, 29);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(187, 31);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Make Payment";
+            this.radiocash.UseVisualStyleBackColor = false;
             // 
             // pictureBox3
             // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox3.Image = global::Store.Properties.Resources.cash_on_delivery;
-            this.pictureBox3.Location = new System.Drawing.Point(536, 206);
+            this.pictureBox3.Location = new System.Drawing.Point(777, 427);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(77, 50);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -137,8 +116,9 @@
             // 
             // pictureBox2
             // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = global::Store.Properties.Resources.BKash_Icon_Logo;
-            this.pictureBox2.Location = new System.Drawing.Point(166, 206);
+            this.pictureBox2.Location = new System.Drawing.Point(407, 427);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(100, 50);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -147,8 +127,9 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::Store.Properties.Resources.Nagad_Logo;
-            this.pictureBox1.Location = new System.Drawing.Point(345, 206);
+            this.pictureBox1.Location = new System.Drawing.Point(586, 427);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 50);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -157,15 +138,12 @@
             // 
             // PaymentForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = global::Store.Properties.Resources.payment;
+            this.ClientSize = new System.Drawing.Size(1226, 715);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.radiocash);
             this.Controls.Add(this.radioNagad);
             this.Controls.Add(this.radioBkash);
@@ -190,9 +168,6 @@
         private System.Windows.Forms.RadioButton radioBkash;
         private System.Windows.Forms.RadioButton radioNagad;
         private System.Windows.Forms.RadioButton radiocash;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox1;
