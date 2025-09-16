0e89fef925a3cf564b75ae4bccee3ab09e9a5c60
Mrx™ <itzrakib34@gmail.com>
2025-09-17 03:09:22 +0600

    registration done fronted

--- Forms/RegistrationForm.Designer.cs
+++ Forms/RegistrationForm.Designer.cs
@@ -28,13 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RegistrationForm));
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
             this.usernametext = new System.Windows.Forms.TextBox();
             this.emailtext = new System.Windows.Forms.TextBox();
             this.passwordBox = new System.Windows.Forms.TextBox();
@@ -44,125 +37,58 @@
             this.registerbutton = new System.Windows.Forms.Button();
             this.cancelbutton = new System.Windows.Forms.Button();
             this.rolecomboBox = new System.Windows.Forms.ComboBox();
-            this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(370, 139);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 16);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "User Name";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.ForeColor = System.Drawing.Color.Transparent;
-            this.label2.Location = new System.Drawing.Point(369, 183);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 16);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Email Address";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.Transparent;
-            this.label3.ForeColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(369, 231);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(67, 16);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Password";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(371, 278);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(115, 16);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Confirm Password";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.Color.Transparent;
-            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label5.Location = new System.Drawing.Point(369, 323);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(58, 16);
-            this.label5.TabIndex = 4;
-            this.label5.Text = "Address";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.BackColor = System.Drawing.Color.Transparent;
-            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label6.Location = new System.Drawing.Point(371, 370);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(99, 16);
-            this.label6.TabIndex = 5;
-            this.label6.Text = "Moblie Number";
             // 
             // usernametext
             // 
             this.usernametext.BackColor = System.Drawing.Color.White;
-            this.usernametext.Location = new System.Drawing.Point(555, 134);
+            this.usernametext.Location = new System.Drawing.Point(455, 239);
             this.usernametext.Name = "usernametext";
-            this.usernametext.Size = new System.Drawing.Size(336, 22);
+            this.usernametext.Size = new System.Drawing.Size(573, 22);
             this.usernametext.TabIndex = 6;
             // 
             // emailtext
             // 
-            this.emailtext.Location = new System.Drawing.Point(555, 183);
+            this.emailtext.Location = new System.Drawing.Point(455, 291);
             this.emailtext.Name = "emailtext";
-            this.emailtext.Size = new System.Drawing.Size(336, 22);
+            this.emailtext.Size = new System.Drawing.Size(573, 22);
             this.emailtext.TabIndex = 7;
             // 
             // passwordBox
             // 
-            this.passwordBox.Location = new System.Drawing.Point(555, 231);
+            this.passwordBox.Location = new System.Drawing.Point(455, 345);
             this.passwordBox.Name = "passwordBox";
-            this.passwordBox.Size = new System.Drawing.Size(336, 22);
+            this.passwordBox.Size = new System.Drawing.Size(573, 22);
             this.passwordBox.TabIndex = 8;
             // 
             // confirmtext
             // 
-            this.confirmtext.Location = new System.Drawing.Point(555, 272);
+            this.confirmtext.Location = new System.Drawing.Point(455, 403);
             this.confirmtext.Name = "confirmtext";
-            this.confirmtext.Size = new System.Drawing.Size(336, 22);
+            this.confirmtext.Size = new System.Drawing.Size(573, 22);
             this.confirmtext.TabIndex = 9;
             // 
             // Addresstext
             // 
-            this.Addresstext.Location = new System.Drawing.Point(555, 320);
+            this.Addresstext.Location = new System.Drawing.Point(455, 455);
             this.Addresstext.Name = "Addresstext";
-            this.Addresstext.Size = new System.Drawing.Size(336, 22);
+            this.Addresstext.Size = new System.Drawing.Size(573, 22);
             this.Addresstext.TabIndex = 10;
             // 
             // mobiletext
             // 
-            this.mobiletext.Location = new System.Drawing.Point(555, 367);
+            this.mobiletext.Location = new System.Drawing.Point(455, 507);
             this.mobiletext.Name = "mobiletext";
-            this.mobiletext.Size = new System.Drawing.Size(336, 22);
+            this.mobiletext.Size = new System.Drawing.Size(573, 22);
             this.mobiletext.TabIndex = 11;
             // 
             // registerbutton
             // 
             this.registerbutton.BackColor = System.Drawing.Color.Lime;
-            this.registerbutton.Location = new System.Drawing.Point(531, 583);
+            this.registerbutton.Location = new System.Drawing.Point(591, 607);
             this.registerbutton.Name = "registerbutton";
-            this.registerbutton.Size = new System.Drawing.Size(218, 46);
+            this.registerbutton.Size = new System.Drawing.Size(183, 42);
             this.registerbutton.TabIndex = 12;
             this.registerbutton.Text = "Register";
             this.registerbutton.UseVisualStyleBackColor = false;
@@ -172,7 +98,7 @@
             // 
             this.cancelbutton.BackColor = System.Drawing.Color.Red;
             this.cancelbutton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.cancelbutton.Location = new System.Drawing.Point(1046, 28);
+            this.cancelbutton.Location = new System.Drawing.Point(1031, 121);
             this.cancelbutton.Name = "cancelbutton";
             this.cancelbutton.Size = new System.Drawing.Size(151, 45);
             this.cancelbutton.TabIndex = 13;
@@ -183,21 +109,10 @@
             // rolecomboBox
             // 
             this.rolecomboBox.FormattingEnabled = true;
-            this.rolecomboBox.Location = new System.Drawing.Point(555, 414);
+            this.rolecomboBox.Location = new System.Drawing.Point(455, 562);
             this.rolecomboBox.Name = "rolecomboBox";
-            this.rolecomboBox.Size = new System.Drawing.Size(336, 24);
+            this.rolecomboBox.Size = new System.Drawing.Size(573, 24);
             this.rolecomboBox.TabIndex = 14;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.BackColor = System.Drawing.Color.Transparent;
-            this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label7.Location = new System.Drawing.Point(372, 414);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(36, 16);
-            this.label7.TabIndex = 15;
-            this.label7.Text = "Role";
             // 
             // label8
             // 
@@ -215,10 +130,9 @@
             // RegistrationForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImage = global::Store.Properties.Resources.register;
             this.ClientSize = new System.Drawing.Size(1317, 728);
             this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
             this.Controls.Add(this.rolecomboBox);
             this.Controls.Add(this.cancelbutton);
             this.Controls.Add(this.registerbutton);
@@ -228,12 +142,6 @@
             this.Controls.Add(this.passwordBox);
             this.Controls.Add(this.emailtext);
             this.Controls.Add(this.usernametext);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "RegistrationForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "RegistrationForm";
@@ -244,13 +152,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox usernametext;
         private System.Windows.Forms.TextBox emailtext;
         private System.Windows.Forms.TextBox passwordBox;
@@ -260,7 +161,6 @@
         private System.Windows.Forms.Button registerbutton;
         private System.Windows.Forms.Button cancelbutton;
         private System.Windows.Forms.ComboBox rolecomboBox;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
     }
 }
